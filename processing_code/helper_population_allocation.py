c84bebb5f2cd2ed4228aa29dd47b878c853bf8a8
Jameson Carter <91226536+jameson-c@users.noreply.github.com>
2022-12-04 12:20:52 -0500

    Check it

--- processing_code/helper_population_allocation.py
+++ processing_code/helper_population_allocation.py
@@ -1,9 +1,6 @@
 """ 
 This script creates a dataset that has the population estimate for each building in our buildings dataset. 
-
-NOTE:
-Currently, since I don't fully understand the mapping of geoids in ACS to geoids in buildings dataset, I am randomly allocating population, to create a pipeline. This needs to be modified soon. 
-Notes
+Authors: Jamie + Abhilash
 """
 # Import libraries
 import geopandas as gpd
@@ -26,6 +23,9 @@ def get_population(geopandas_dataframe):
     merge.loc[merge['class_reco'] == '2-Unit Residential', 'population'] = 2
     merge.loc[merge['class_reco'] == '3-Unit Residential', 'population'] = 3
     merge.loc[merge['class_reco'] == '4+ Unit Residential', 'population'] = 4
+    # Recode missing to whatever the county average is
+    # Obtained: https://data.census.gov/table?text=B25010_001E&g=0500000US42003&tid=ACSDT1Y2021.B25010
+    merge.loc[merge['B25010_001E'].isna(), 'B25010_001E'] = 2.2
     # multiply by average household size
     merge['population'] = merge['population']*merge['B25010_001E'] 
 
